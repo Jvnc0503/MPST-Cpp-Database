9ba79544b975d580537e1c3ada99c31c2319e83b
Jvnc0503 <jvnc0503@gmail.com>
2024-07-14 20:03:52 -0500

    commit

---
+++ UserManager.h
@@ -0,0 +1,49 @@
+#ifndef USERMANAGER_H
+#define USERMANAGER_H
+
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include "Movie.h"
+using std::unordered_set, std::unordered_map, std::string;
+
+class UserManager {
+    static UserManager* instance;
+    unordered_set<int> watchLater;
+    unordered_set<int> likes;
+    unordered_map<string, int> tagPreferences;
+
+    UserManager() = default;
+public:
+    static UserManager* getInstance() {
+        if (!instance) {
+            instance = new UserManager();
+        }
+        return instance;
+    }
+
+    void addToWatchLater(const Movie& movie) {
+        watchLater.emplace(movie.getId());
+    }
+
+    void addToLikes(const Movie& movie) {
+        likes.emplace(movie.getId());
+        for (const auto& tag : movie.getTags()) {
+            tagPreferences[tag]++;
+        }
+    }
+
+    const unordered_set<int>& getWatchLater() const {
+        return watchLater;
+    }
+
+    const unordered_set<int>& getLikes() const {
+        return likes;
+    }
+
+    const unordered_map<string, int>& getTagPreferences() const {
+        return tagPreferences;
+    }
+};
+
+#endif
